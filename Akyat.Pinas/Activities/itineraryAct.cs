a8c21b64dcd1e8937ef5193af1e3aac7212ac535
King James <King James@DESKTOP-PAUDAHV>
2016-10-03 21:35:09 +0800

    onting codes

--- Akyat.Pinas/Activities/itineraryAct.cs
+++ Akyat.Pinas/Activities/itineraryAct.cs
@@ -19,6 +19,7 @@ namespace Akyat.Pinas.ORM
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            
             SetContentView(Resource.Layout.itineraryLayout);
             // Create your application here
 
